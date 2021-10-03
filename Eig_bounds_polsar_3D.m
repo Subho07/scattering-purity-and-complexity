ffbbabbd6fa4ce64494c9dbe1b4cdc8c903e98e9
Subhadip Dey <sdey2307@gmail.com>
2021-10-03 23:54:21 +0530

    Update Eig_bounds_polsar_3D.m

--- Eig_bounds_polsar_3D.m
+++ Eig_bounds_polsar_3D.m
@@ -1,3 +1,36 @@
+###############################################################################
+#  Eig_bounds_polsar_3D.m
+#
+#  Project:
+#  Purpose:  Estimation of scattering purity and complexity from polarimetric SAR data
+#  Author:   Avik Bhattacharya and Subhadip Dey
+#  Emails:   avik.bhattach@gmail.com and sdey2307@gmail.com
+#  Created:  October 2021
+#
+###############################################################################
+#  Copyright (c) 2021, Avik Bhattacharya and Subhadip Dey
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included
+#  in all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+
+
 [filename, path] = uigetfile('*.txt', 'Select Full Pol config file');
 FolderName = path;
 
